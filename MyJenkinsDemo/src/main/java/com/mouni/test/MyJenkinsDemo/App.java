2d5e2dd30526b4d1dbea5d794ed4d53579262541
Jagadeesh <jagadeesh.ifs@gmail.com>
2017-03-06 00:04:51 +0530

    first commit

---
+++ MyJenkinsDemo/src/main/java/com/mouni/test/MyJenkinsDemo/App.java
@@ -0,0 +1,13 @@
+package com.mouni.test.MyJenkinsDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
